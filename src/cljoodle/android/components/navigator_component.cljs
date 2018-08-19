5e3d5bdf1383278bcd1029ad8a428a2c4be42f83
Pawel Jankowski <pawel.jankowski@spagee.com>
2018-08-19 16:59:51 +0200

    navigator component ready

--- src/cljoodle/android/components/navigator_component.cljs
+++ src/cljoodle/android/components/navigator_component.cljs
@@ -5,11 +5,20 @@
 
 
 (defn navigator-component
-  []
+  [title]
   [comm/view {:style {:flex-direction  "row"
                       :align-items     "center"
-                      :backgroundColor "aliceblue"}}
-   [comm/text "foo"]
-   [comm/text {:style {:flex-grow 1}} ""]
-   [comm/text "foo"]
+                      :justify-content "space-between"
+                      :backgroundColor "#0066cc"
+                      :height          50}}
+   [comm/button {:style   {}
+                 :title   "back"
+                 :onPress #(prn "1")}]
+   [comm/text {:style     {:text-align  "center"
+                           :font-weight "bold"
+                           :color       "#f0f8ff"}
+               :flex-grow 1} title]
+   [comm/button {:style   {}
+                 :title   "menu"
+                 :onPress #(prn "2")}]
    ])
