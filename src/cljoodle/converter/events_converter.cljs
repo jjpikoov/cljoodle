3a01414a453717bcaa4573052fe836623c179cd0
Pawel Jankowski <pawel.jankowski@spagee.com>
2018-08-24 20:55:44 +0200

    Removed debug snippet from events_converter.cljs

--- src/cljoodle/converter/events_converter.cljs
+++ src/cljoodle/converter/events_converter.cljs
@@ -7,11 +7,6 @@
 
 (defn date_to_epoch_int
   [year month day]
-  (prn (str year
-            ""
-            month
-            ""
-            day))
   (/ (.getTime (js/Date. year, (dec month), day)) 1000))
 
 (defn convert-event-to-menu-item-format
